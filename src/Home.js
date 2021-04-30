427b7dad1a3bff71e6ae7de8ba82212734a67139
Jeremiah Long <jml@jmldev.com>
2021-04-29 20:35:16 -0700

    slowed typewriter and did some npm updates

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ class Home extends Component {
     }
 
     typeWriter = () => {
-        let speed = 10;
+        let speed = 20;
         let tempText = this.state.content;
         let text = "Howdy. My name is Jeremiah Long. Micah is my middle name and there's the letters in JMLdev.com. I've been creating websites for a long time. I started right after we ironed out the bugs for Y2K. Since then I've managed to get pretty good at a few things. JavaScript for one, HTML and CSS of course, then a bunch of other cool stuff like React, Node.js, MERN stack... I could go on for a long time, but I think I'll just create a page for those skills. Anyways, nice to meet you. Feel free to look around and get to know me a little better.";
         if (this.state.content.length < text.length) {
