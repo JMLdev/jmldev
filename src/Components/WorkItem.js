907a5ad4607862fcb25813c06af6f8889e6f46a1
Jeremiah Long <jml@jmldev.com>
2022-01-03 20:36:58 -0800

    removed expand because funky on mobile

--- src/Components/WorkItem.js
+++ src/Components/WorkItem.js
@@ -15,12 +15,12 @@ export default class WorkItem extends React.Component {
         const isActive = this.state.active;
         this.setState({
             active: !isActive
-        })
+        });
     }
 
     render() {
         return (
-            <Col xs={12} md={6} className={'work-item ' + (this.state.active ? 'expanded' : '')} onClick={this.expand}>
+            <Col xs={12} md={6} className={'work-item ' + (this.state.active ? 'expanded' : '')}>
                 <div className="exit">X</div>
                 <h3>{this.props.headline}</h3>
                 <img src={this.props.image} alt={this.props.alt} />
